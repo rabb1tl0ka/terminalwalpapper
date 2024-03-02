b596f94012bd45c8011ab87613617819753efc9f
Rabb1tL0ka <bruno@loka.com>
2024-03-02 19:09:01 +0000

    Activating the venv in py

--- termwpcolors.py
+++ termwpcolors.py
@@ -1,6 +1,11 @@
 import subprocess
 import os
 
+def activate_venv():
+    venv_activate_script = os.path.join(os.path.dirname(__file__), '.venv', 'bin', 'activate')
+    print(f"activating venv: {venv_activate_script}")
+    subprocess.run(['source', venv_activate_script], shell=True)
+
 # Get the current wallpaper file URI using gsettings
 uri = subprocess.check_output(['gsettings', 'get', 'org.gnome.desktop.background', 'picture-uri']).decode().strip()
 
@@ -12,6 +17,8 @@ uri = uri.strip("'")
 
 #print("wal -i "+uri)
 print(uri)
+
+activate_venv()
 
 # Execute the wal command with the wallpaper file URI
 subprocess.run(['wal', '-i', os.path.expanduser(uri)])
